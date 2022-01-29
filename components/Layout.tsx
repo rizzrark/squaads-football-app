57ca1b1ebed48b888169d48ec2d01a19a1e368c3
rizzrark <razzilsp@gmail.com>
2022-01-29 18:14:09 -0500

    feat: Add `Link` component to `Navigation` component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Navigation from './Navigation'
+import DummyTextComponent from './DummyTextComponent'
 
 type Props = {
   children?: ReactNode
@@ -18,9 +19,10 @@ const Layout: React.FC<Props> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>Title</header>
+    <header className="fixed top-0">Title</header>
     <main className="flex flex-col items-center justify-center ">
       {children}
+      <DummyTextComponent />
     </main>
     <Navigation />
   </div>
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,30 @@
+import Link from 'next/link'
+
 const Navigation: React.FC = () => (
-  <nav className="fixed bottom-0">Navigation component</nav>
+  <nav className="fixed bottom-0 flex w-full h-16 m-0 shadow-lg sm:top-0 bg-slate-200">
+    <ul className="flex items-center w-full justify-evenly">
+      <li>
+        <Link href="/">
+          <a>
+            <i>c</i>
+          </a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <a>
+            <i>c</i>
+          </a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <a>
+            <i>c</i>
+          </a>
+        </Link>
+      </li>
+    </ul>
+  </nav>
 )
 export default Navigation
