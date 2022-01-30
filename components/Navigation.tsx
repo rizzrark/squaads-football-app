ec803d58fbad83d2db684658b523c458940a5629
rizzrark <razzilsp@gmail.com>
2022-01-30 10:51:38 -0500

    feat: Add active link status to `Navigation` component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { IoIosFootball, IoMdPerson } from 'react-icons/io'
 import { BsFillPeopleFill } from 'react-icons/bs'
 
@@ -13,33 +14,39 @@ const Navigation: React.FC<NavigationProps> = ({
   navHomeIconSize,
   navIconSize,
   navColor,
-}: NavigationProps) => (
-  <nav
-    className={`fixed bottom-0 flex w-full h-16 m-0 shadow-lg sm:top-0 ${navColor} `}
-  >
-    <ul className="flex items-center w-full justify-evenly">
-      <li>
-        <Link href="/teams">
-          <a>
-            <BsFillPeopleFill size={navIconSize} />
-          </a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/">
-          <a>
-            <IoIosFootball size={navHomeIconSize} />
-          </a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/players">
-          <a>
-            <IoMdPerson size={navIconSize} />
-          </a>
-        </Link>
-      </li>
-    </ul>
-  </nav>
-)
+}: NavigationProps) => {
+  const router = useRouter()
+  const active = 'bg-white'
+
+  return (
+    <nav
+      className={`fixed bottom-0 flex w-full h-16 m-0 shadow-lg sm:top-0 ${navColor}`}
+    >
+      <ul className="flex items-center w-full justify-evenly">
+        <li className={router.pathname === '/teams' ? active : ''}>
+          <Link href="/teams">
+            <a>
+              <BsFillPeopleFill size={navIconSize} />
+            </a>
+          </Link>
+        </li>
+        <li className={router.pathname === '/' ? active : ''}>
+          <Link href="/">
+            <a>
+              <IoIosFootball size={navHomeIconSize} />
+            </a>
+          </Link>
+        </li>
+        <li className={router.pathname === '/players' ? active : ''}>
+          <Link href="/players">
+            <a>
+              <IoMdPerson size={navIconSize} />
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
 export default Navigation
