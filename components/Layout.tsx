c7d90eb21971dc2ec105e2db6b701663410b7f7a
rizzrark <razzilsp@gmail.com>
2022-01-30 09:37:38 -0500

    refactor: Remove title property from `Layout` component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,13 +8,9 @@ type Props = {
   title?: string
 }
 
-const Layout: React.FC<Props> = ({
-  children,
-  title = 'This is the default title',
-}: Props) => (
+const Layout: React.FC<Props> = ({ children }: Props) => (
   <div>
     <Head>
-      <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
