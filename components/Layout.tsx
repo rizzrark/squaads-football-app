5627556cf4faabbfac18e601b58397b46db28ee4
rizzrark <razzilsp@gmail.com>
2022-01-30 16:01:03 -0500

    style: Add padding to `main` in `Layout` component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -34,7 +34,7 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
         navIconSize={32}
         navColor="bg-neutral-800"
       />
-      <main className="flex flex-col items-center justify-center mx-4 ">
+      <main className="flex flex-col items-center justify-center pt-4 pb-20 mx-4">
         {children}
         <DummyTextComponent />
       </main>
