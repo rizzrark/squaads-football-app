ec506de90a835c5218ac32646739d0b1f988b8c7
rizzrark <razzilsp@gmail.com>
2022-02-01 12:26:38 -0500

    style: Add styles to `.pagetitle` custom class

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -7,8 +7,8 @@ export const PageTitle: React.FC<PageTitleProps> = ({
   pageTitle,
   pageTitleColor,
 }: PageTitleProps) => (
-  <div className={`${pageTitleColor} pagetitle `}>
-    <h1 className="text-2xl font-extrabold tracking-wider text-center">
+  <div className={`${pageTitleColor} pagetitle  `}>
+    <h1 className="text-2xl font-extrabold tracking-wider text-center ">
       {pageTitle}
     </h1>
   </div>
